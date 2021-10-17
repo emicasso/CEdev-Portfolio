c5e90af2da34a57e358f4a895630ab72a465e4e8
emiliano cassoni <cassoniemiliano@gmail.com>
2021-10-17 19:09:38 -0300

    Presentation of my projects in cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Describe from "./Describe";
 import Button from "react-bootstrap/Button";
-import './Cards.css'
+import "./Cards.css";
+import "aos/dist/aos.css";
+import Aos from "aos";
 
 import AquariumPage from "../media/Acuarium_Trim.mp4";
 import cedevPage from "../media/cedev_Trim.mp4";
 import DviajePage from "../media/DviajePage.png";
 import florbyfranPage from "../media/fbf_Trim.mp4";
 import { Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const cards = [
   {
@@ -67,33 +70,44 @@ const cards = [
 ];
 
 function Cards() {
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
+
   return (
-    <div className="container-fluid cards__container align-items-center py-5">
+    <div className="container-fluid cards__container align-items-center py-5" data-aos="fade-up">
       <div className="container">
         <div className="row align-items-center justify-content-center text-center pt-5">
           <h1>PROJECT</h1>
           <span>This is a summary of my personal projects.</span>
-          <p>Hope you like them and we can work together <span>&#129309;</span>.</p>
+          <p>
+            Hope you like them and we can work together <span>&#129309;</span>.
+          </p>
         </div>
-        <div className="row align-items-center justify-content-center py-3">
-          {cards.map((card) => (
-            <div className="col-sm my-2" key={card.id}>
-              <Describe
-                name={card.name}
-                imageSrc={card.image}
-                Autor={card.autor}
-                Description={card.description}
-                Technologies={card.technologies}
-                Url={card.url}
-              />
-            </div>
-          ))}
+        <div className="container">
+          <div className="row align-items-center justify-content-center py-3">
+            {cards.map((card) => (
+              <div className="col-sm my-2" data-aos="zoom-in" key={card.id}>
+                <Describe
+                  name={card.name}
+                  imageSrc={card.image}
+                  Autor={card.autor}
+                  Description={card.description}
+                  Technologies={card.technologies}
+                  Url={card.url}
+                />
+              </div>
+            ))}
+          </div>
         </div>
+
         <div className="row text-center">
-          <Container>
-            <Button variant="outline-dark" size="lg">
-              View all Project
-            </Button>
+          <Container data-aos="zoom-in-up">
+            <Link to="/projects">
+              <Button variant="outline-dark" size="lg">
+                View all Project
+              </Button>
+            </Link>
           </Container>
         </div>
       </div>
