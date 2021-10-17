beb6d82f5bdd9cff0353f4727ae482f18fb520fc
emiliano cassoni <cassoniemiliano@gmail.com>
2021-10-17 19:04:35 -0300

    Projects Page

--- src/components/pages/Projects/Cards.js
+++ src/components/pages/Projects/Cards.js
@@ -9,8 +9,10 @@ import DviajePage from "../../../media/DviajePage.png";
 import DviajeSlide from "../../../media/DviajeSlide.png";
 import florbyfranPage from "../../../media/florbyfranPage.png";
 import florbyfranSlide from "../../../media/florbyfranSlide.png";
-// import brithdayPage from "../../../media/brithdayPage.png";
-// import brithdaySlide from "../../../media/brithdaySlide.png";
+import brithdayPage from "../../../media/brithdayPage.png";
+import brithdaySlide from "../../../media/brithdaySlide.png";
+import CotizadorPage from "../../../media/CotizadorPage.png";
+import CotizadorSlide from "../../../media/CotizadorSlide.png";
 
 const project = [
   {
@@ -20,8 +22,7 @@ const project = [
     imgS: AcuariumSlide,
     create: 2020,
     autor: ["CE{dev}"],
-    description:
-      "Ecommerce dedicado a la venta de peces ornamentales, materiales de cuidado, peceras personzalidas, etc...",
+    description:"Ecommerce dedicated to the sale of ornamental fish, care materials, fish tanks personzalidas. Created and designed for Acuarium-Posadas Argentina client. (Also presented as a final project in the Faculty where I study)",
     technologies: [
       "PHP",
       "CODEIGNITER",
@@ -41,8 +42,7 @@ const project = [
     name: "CE{Dev}",
     autor: ["CE{dev}"],
     description:
-      "Created by me this is my Web Portfolio in React JS based on various projects",
-    technologies: [
+      "Creation of my Portfolio in ReactJs in which I personally design based on my tastes and knowledge. My Personal Projects and My Data are located",    technologies: [
       "REACT JS",
       "JS",
       "BOOTSTRAP",
@@ -50,7 +50,7 @@ const project = [
       "CSS",
       "REACT ROUTER",
     ],
-    url: "https://github.com/emicasso/Aquarium-Posadas",
+    url: "https://github.com/emicasso/CE-dev-",
   },
   {
     id: 3,
@@ -60,9 +60,9 @@ const project = [
     name: "Flor By Fran",
     autor: ["CE{dev}"],
     description:
-      "Hecho para un nuevo emprendimiento con marca registrada de venta de ropa y accesorios",
+      "Made for a new venture selling clothes and accessories, it was also one of my first front-end projects.",
     technologies: ["HTLM", "CSS", "BOOTSTRAP", "JS", "FLEX-BOX", ""],
-    url: "https://github.com/emicasso/Aquarium-Posadas",
+    url: "https://github.com/emicasso/FlorByFran",
   },
   {
     id: 4,
@@ -71,10 +71,32 @@ const project = [
     create: 2021,
     name: "Dviaje",
     autor: ["CE{dev}"],
-    description: "ReactJS Web for traveling enterprise.",
+    description: "Website made in ReactJS. As a tutotial taken from Youtube but with many personal modifications of their own to expand knowledge",
     technologies: ["REACT JS", "REACK HOOKS", "BOOTSTRAP", "CSS", "HTML"],
-    url: "https://github.com/emicasso/Aquarium-Posadas",
+    url: "https://github.com/emicasso/Dviaje",
   },
+  {
+    id: 5,
+    imgP: brithdayPage,
+    imgS: brithdaySlide,
+    create: 2021,
+    name: "Brithday",
+    autor: ["CE{dev}"],
+    description: "User upload app to schedule birthdays. Entry and Creations of users so that each one has a unique database with its own agenda",
+    technologies: ["REACT NATIVE", "REACK HOOKS", "ANDROID STUDIO", "FIREBASE", "HTML"],
+    url: "https://github.com/emicasso/Brithday",
+  },
+  {
+    id: 6,
+    imgP: CotizadorPage,
+    imgS: CotizadorSlide,
+    create: 2021,
+    name: "Cotizador Prestamos",
+    autor: ["CE{dev}"],
+    description: "Loan quote, where the user can enter the amount he wants to request in the loan, the interest and the terms in which he will pay.",
+    technologies: ["REACT NATIVE", "REACK HOOKS", "ANDROID STUDIO", "CSS", "HTML"],
+    url: "https://github.com/emicasso/CotizadordePrestamos",
+  }
 ];
 
 function Cards() {
--- src/components/pages/Projects/MyProject.js
+++ src/components/pages/Projects/MyProject.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Button } from "react-bootstrap";
 import "./CardProjects.css";
+import "aos/dist/aos.css";
+import Aos from "aos";
 
 function MyProject(project) {
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
+
   return (
     <>
-      <div className="movie_card" id="bright">
+      <div
+        className="movie_card" data-aos="fade-up"
+        id="bright"
+      >
         <div className="info_section">
           <div className="movie_header">
             <img src={project.imageS} className="locandina" alt="" />
@@ -15,34 +25,63 @@ function MyProject(project) {
           </div>
           <div className="movie_desc">
             <p className="text">{project.Description}</p>
+            <Button variant="outline-dark">
+              {" "}
+              <a
+                href={project.Url}
+                target="_blank"
+                rel="noreferrer"
+                style={{ textDecoration: "none", color: "white" }}
+              >
+                Demo
+              </a>
+            </Button>
           </div>
           <div className="container mb-2">
-            <span className="border border-secondary px-1 rounded mx-1 " style={{ color: "#757171" }}>
+            <span
+              className="border border-secondary px-1 rounded mx-1 "
+              style={{ color: "#757171" }}
+            >
               {project.Technologies[0]}
             </span>
-            <span className="border border-secondary px-1 rounded mx-1" style={{ color: "#757171" }}>
+            <span
+              className="border border-secondary px-1 rounded mx-1"
+              style={{ color: "#757171" }}
+            >
               {project.Technologies[1]}
             </span>
             <span
-              className={project.Technologies[2] ? "border border-secondary px-1 rounded mx-1" : "invisible"}
+              className={
+                project.Technologies[2]
+                  ? "border border-secondary px-1 rounded mx-1"
+                  : "invisible"
+              }
               style={{ color: "#757171" }}
             >
               {project.Technologies[2]}
             </span>
             <span
-              className={project.Technologies[3] ? "border border-secondary px-1 rounded mx-1" : "invisible"}
+              className={
+                project.Technologies[3]
+                  ? "border border-secondary px-1 rounded mx-1"
+                  : "invisible"
+              }
               style={{ color: "#757171" }}
             >
               {project.Technologies[3]}
             </span>
             <span
-              className={project.Technologies[4] ? "border border-secondary px-1 rounded mx-1" : "invisible"}
+              className={
+                project.Technologies[4]
+                  ? "border border-secondary px-1 rounded mx-1"
+                  : "invisible"
+              }
               style={{ color: "#757171" }}
             >
               {project.Technologies[4] ? `+` : ""}
             </span>
             <br />
-          </div>          
+          </div>
         </div>
         <div
           className="blur_back "
