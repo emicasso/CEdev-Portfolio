6c8f76adb1f1edb0c78e41cbc2ab294e26c4292b
emiliano cassoni <cassoniemiliano@gmail.com>
2021-10-25 21:16:50 -0300

    SEO aggregation and 404 page error

--- src/App.js
+++ src/App.js
@@ -6,18 +6,24 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { faCheckSquare, faCoffee } from "@fortawesome/free-solid-svg-icons";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 import ScrollToTop from "./components/ScrollToTop";
 import Home from "./components/pages/HomePage/Home";
 import Projects from "./components/pages/Projects/Projects";
 import AboutMe from "./components/pages/AboutMe/About";
 import Contact from "./components/pages/Contact/Contact";
+import { Helmet } from "react-helmet";
+import PageNotFound from "./components/pages/PageNotFound";
 
 library.add(fab, fas, faCheckSquare, faCoffee);
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <meta name="description" content="My personal portfolio in reactjs cassoni emiliano" />
+        <meta name="keywords" content="Personal portfolio, autonomous projects and contact information" />
+      </Helmet>
       <Router>
         <ScrollToTop/>
         <Navbar/>
@@ -26,6 +32,7 @@ function App() {
               <Route path="/projects" component={Projects} />
               <Route path="/about-me" component={AboutMe} />
               <Route path="/contact" component={Contact} />
+              <Route component={PageNotFound}/>
             </Switch>
             <Footer/>
       </Router>
