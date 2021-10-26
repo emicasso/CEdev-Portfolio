920f0941490e07b29ed877eda8c2ddf1b3539366
emiliano cassoni <cassoniemiliano@gmail.com>
2021-10-25 21:12:04 -0300

    SEO is added

--- src/components/pages/Projects/Projects.js
+++ src/components/pages/Projects/Projects.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Helmet } from 'react-helmet';
 import "../../../App.css";
 import Cards from './Cards';
 
@@ -6,6 +7,10 @@ import Cards from './Cards';
 function Projects() {
     return (
         <>
+        <Helmet>
+            <title>CEdev Projects</title>
+            <meta name="description" content="Explore my new personal projects" />
+        </Helmet>
             <Cards/>
         </>
     )
